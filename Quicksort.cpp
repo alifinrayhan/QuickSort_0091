f8451dd5932cc59627b643db3ab16c31a1f09648
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-03-29 14:06:46 +0700

    commit 2

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -7,3 +7,26 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
+void input(){
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+           cout << "\nMaksimum panjang array adalah 20" << endl;
+
+    }
+    cout << "\n----------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n----------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+//swaps the element at index x with the element at index y   
